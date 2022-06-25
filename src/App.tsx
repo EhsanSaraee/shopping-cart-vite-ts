083e1f2f74731bbd2bfeee54a06b1abc2484ea19
ehsan <ehsansaraee6570@yahoo.com>
2022-06-25 07:51:01 +0430

    create navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { Container } from 'react-bootstrap';
 import { Route, Routes } from 'react-router-dom';
 import { About, Home, Store } from 'pages';
+import { NavbarComp } from 'components';
 
 const App = () => {
    return (
-      <Container>
-         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/store" element={<Store />} />
-            <Route path="/about" element={<About />} />
-         </Routes>
-      </Container>
+      <>
+         <NavbarComp />
+         <Container className="mb-4">
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="/store" element={<Store />} />
+               <Route path="/about" element={<About />} />
+            </Routes>
+         </Container>
+      </>
    );
 };
 
