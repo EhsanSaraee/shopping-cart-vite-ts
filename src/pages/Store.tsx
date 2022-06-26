2496cec049c07603e4eee1f989833323affae00c
ehsan <ehsansaraee6570@yahoo.com>
2022-06-26 20:08:53 +0430

    create store page & storeItems

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,18 @@
+import { StoreItem } from 'components/StoreItem';
+import { Col, Row } from 'react-bootstrap';
+import storeData from '../data/items.json';
+
 export const Store = () => {
    return (
       <>
          <h1>Store</h1>
+         <Row xs={1} md={2} lg={3} className="g-3">
+            {storeData?.map((item) => (
+               <Col key={item.id}>
+                  <StoreItem {...item} />
+               </Col>
+            ))}
+         </Row>
       </>
    );
 };
